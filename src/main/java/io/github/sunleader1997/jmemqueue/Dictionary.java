99a2280764f131473f245869f6f6b182a36af826
Sunleader <sunyaxing_1997@163.com>
2026-01-19 15:30:34 +0800

    补充linux 测试基准

--- src/main/java/io/github/sunleader1997/jmemqueue/Dictionary.java
+++ src/main/java/io/github/sunleader1997/jmemqueue/Dictionary.java
@@ -3,7 +3,7 @@ package io.github.sunleader1997.jmemqueue;
 import java.io.File;
 
 public class Dictionary {
-    public static final String PARENT_DIR = System.getProperty("java.io.tmpdir") + "JSMQ" + File.separator;
+    public static final String PARENT_DIR = System.getProperty("java.io.tmpdir") + File.separator + "JSMQ" + File.separator;
 
     static {
         new File(PARENT_DIR).mkdir();
